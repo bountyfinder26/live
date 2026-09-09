0c41749a3edf1bc6c2a45c72bd3aeedc29e4c951
bountyfinder26 <324511660+bountyfinder26@users.noreply.github.com>
2026-09-09 11:08:34 +0530

    Add MX Master 3S Grant to shop items

--- src/lib/shopItems.ts
+++ src/lib/shopItems.ts
@@ -29,7 +29,7 @@ export const allShopItems: ShopItem[] = [
   { name: "Anker Nano Charger (100W) with USB-C Cable", price: 15, img: "/prizes/anker_image.png", description: "best charger ever" },
   { name: "One Key Keychain", price: 2, img: "/prizes/keychain_image.jpg", description: "CLICKY CLICKY CLICKY" },
   { name: "Four Key Macropad", price: 4, img: "/prizes/macropad_image.jpg", description: "little macropad you can actually use" },
-
+{ name: "$70 MX Master 3S Grant", price: 12, img: "/prizes/mouse.png", description: "$70 for mx master 3s" },
   // ===== 17 HOURS =====
   { name: "YubiKey 5C NFC", price: 20, img: "/prizes/yubikey.jpg", description: "secure your shi" },
 
